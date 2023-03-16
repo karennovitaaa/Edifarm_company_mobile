0395a8a47e9337dc58de65b197cf35f1cb248372
Lusyyy <lusyyd1@gmail.com>
2023-03-17 01:01:33 +0700

    bubble chat

---
+++ app/src/main/java/com/aditiyagilang/edifarm_company/aktivitas.java
@@ -0,0 +1,14 @@
+package com.aditiyagilang.edifarm_company;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aktivitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktivitas);
+    }
+}
