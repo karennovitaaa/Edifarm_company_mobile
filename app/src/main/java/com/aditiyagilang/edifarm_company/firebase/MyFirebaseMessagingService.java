4af9bbebf659b4076ef521f070a67fda905c0ef9
aditiyagilang <aditiyagilang031@gmail.com>
2023-05-23 21:19:00 +0700

    au notif guysss kereeeen

--- app/src/main/java/com/aditiyagilang/edifarm_company/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/aditiyagilang/edifarm_company/firebase/MyFirebaseMessagingService.java
@@ -14,7 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
 import com.aditiyagilang.edifarm_company.R;
-import com.aditiyagilang.edifarm_company.dashboardfixx.dashboardfix;
+import com.aditiyagilang.edifarm_company.login;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -24,39 +24,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-            if (remoteMessage.getNotification() != null) {
-                Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-                sendNotification(remoteMessage.getNotification().getBody());
-            }
 
         }
-
-        // Check if message contains a notification payload.
-
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getBody());
+        }
     }
+
 
     @Override
     public void onNewToken(@NonNull String token) {
         Log.d(TAG, "Refreshed token: " + token);
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
-//        sendRegistrationToServer(token);
     }
 
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, dashboardfix.class);
+        Intent intent = new Intent(this, login.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_IMMUTABLE);
@@ -65,8 +55,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.mipmap.logo_edifarms)
-                        .setContentTitle("FCM Message")
+                        .setSmallIcon(R.drawable.edifarm_logo)
+                        .setContentTitle("EDIFARM")
                         .setContentText(messageBody)
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
